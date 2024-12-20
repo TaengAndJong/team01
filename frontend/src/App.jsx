378f5a438128e022b596c73c26c0b61162bef709
JongHoSke <astra9706@naver.com>
2024-12-20 20:36:14 +0900

    충돌 해결 중2 코드 app.jsx 코드 병합

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,6 +54,7 @@ function App() {
                 <Route path="/admin" element={<Admin data={data} setUrl={setUrl}/>} />
                 <Route path="/page" element={<Page data={data} setUrl={setUrl}/>} />
                 <Route path="/signup" element={<SignUp/>} />
+                <Route path="/admin/test" element={<AdminTest data={data} setUrl={setUrl}/>} />
             </Routes>
         </div>
     );
