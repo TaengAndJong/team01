f91b7fe68a8570ad0c9ee0cb2f9b304776e9a483
JongHoSke <astra9706@naver.com>
2024-12-21 18:41:09 +0900

    로그인 폼 작성

---
+++ frontend/src/pages/singUp/components/idForm.jsx
@@ -0,0 +1,36 @@
+import Btn from "../../../util/reuseBtn.jsx";
+import {checkDuplicate, validID} from "../../../util/validation.jsx";
+import {useState} from "react";
+
+const idForm = () => {
+    const [id, setId] = useState('');
+    const [idCheckMessage, setIdCheckMessage] = useState('대소문자, 숫자를 포함한 3~20자 이내');
+    const handleIdChange = async (e) => {
+        const newId = e.target.value;
+        setId(newId);
+        console.log("아이디",newId);
+
+        const validationResult = validID(newId);
+        if(validationResult && validationResult.massage) {
+            setIdCheckMessage(validationResult.massage);
+        } else {
+            setIdCheckMessage('대소문자, 숫자를 포함한 ~20자 이내');
+        }
+    }
+    return(
+        <>
+            <div>
+                <label>아이디 </label>wq
+                <input type="text" name="id" placeholder="아이디를 입력해주세요" onChange={handleIdChange} value={id}
+                       maxLength={20}/>
+                <p>{idCheckMessage}</p>
+                <Btn text="중복확인" type="" onClick={() => {
+                    alert(checkDuplicate(id).massage);
+
+                }}/>
+            </div>
+        </>
+    )
+}
+
+export default idForm;
