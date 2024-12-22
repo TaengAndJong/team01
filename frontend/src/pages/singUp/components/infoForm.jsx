08ad74d44e3d325bc22ed24d557a0fd11c930106
JongHoSke <astra9706@naver.com>
2024-12-22 19:32:04 +0900

    로그인 폼 onChange로 콘솔에서 데이터 확이 가능하게 수정

--- frontend/src/pages/singUp/components/infoForm.jsx
+++ frontend/src/pages/singUp/components/infoForm.jsx
@@ -4,16 +4,43 @@ import DaumPostcode from "../../../util/daumPostcode.jsx";
 const infoForm = ({formData,onInputChange}) =>{
     const [name, setName] = useState('');
 
+
+    // e-mail handle
     const emailChangeHandle = (e) => {
         const newEmail = e.target.value;
         onInputChange("email", newEmail);
     }
 
-    const handleAddressSelect = (address) => {
+    // name handle
+    const nameChangeHandle = (e) => {
+        const newName = e.target.value;
+        onInputChange("name", newName);
+    }
+
+    // phone handle
+    const phoneChangeHandle = (e) => {
+        const newPhone = e.target.value;
+        onInputChange("phone", newPhone);
+    }
+
+    // identiNum handle
+    const f_IdentiNumChangeHandle = (e) => {
+        const newFidentiNum = e.target.value;
+        onInputChange("f_IdentiNum", newFidentiNum);
+    }
+
+    const s_IdentiNumChangeHandle = (e) => {
+        const newSidentiNum = e.target.value;
+        onInputChange("s_IdentiNum", newSidentiNum);
+    }
+
+    // address handle
+    const handleAddressSelect = (address) => { // 우편번호 검색한 데이터를 입력해주는 handler
         onInputChange('adr', address); // adr 필드 업데이트
     };
 
-    const adrHandle = (e) => {
+
+    const adrHandle = (e) => { // 사용자가 직접 입력 할 시 동작하는 handler
         const newAdr = e.target.value;
         onInputChange('adr', newAdr)
     }
@@ -22,30 +49,51 @@ const infoForm = ({formData,onInputChange}) =>{
         const newDtailAdr = e.target.value;
         onInputChange('dtailAdr',newDtailAdr);
     }
+
+
+
     return (
         <>
             <div>사용자 정보 폼</div>
+
+            <div>
+                <label>이메일 </label>
+                <input type="text" name="email" placeholder="ex) text@exemple.com"
+                       value={formData.email}
+                       maxLength={20}
+                       onChange={emailChangeHandle}
+                />
+            </div>
+
             <div>
                 <label>이름 </label>
                 <input type="text" name="name" placeholder="이름을 입력해주세요"
                        value={formData.name}
-                       maxLength={20}/>
+                       maxLength={20}
+                       onChange={nameChangeHandle}
+                />
             </div>
             <div>
                 <label>전화번호 </label>
                 <input type="text" name="phone" placeholder={`"-" 생략해 주세요`}
                        value={formData.phone}
-                       maxLength={20}/>
+                       maxLength={20}
+                       onChange={phoneChangeHandle}
+                />
             </div>
             <div>
                 <label>주민번호 </label>
                 <input type="text" name="f_IdentiNum" placeholder="앞자리 번호를 입력해주세요"
                        value={formData.f_IdentiNum}
-                       maxLength={20}/>
+                       maxLength={20}
+                       onChange={f_IdentiNumChangeHandle}
+                />
                 <span>-</span>
                 <input type="text" name="s_IdentiNum" placeholder="뒷자리 번호를 입력해주세요"
                        value={formData.s_IdentiNum}
-                       maxLength={20}/>
+                       maxLength={20}
+                       onChange={s_IdentiNumChangeHandle}
+                />
             </div>
             <div>
                 <label>주소 </label>
@@ -65,14 +113,6 @@ const infoForm = ({formData,onInputChange}) =>{
                        onChange={dtailAdrHandle}
                 />
 
-            </div>
-            <div>
-                <label>이메일 </label>
-                <input type="text" name="email" placeholder="ex) text@exemple.com"
-                       value={formData.email}
-                       maxLength={20}
-                       onChange={emailChangeHandle}
-                    />
             </div>
 
         </>
