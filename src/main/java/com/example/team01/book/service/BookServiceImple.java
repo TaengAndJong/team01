666e174811aebc72f8d93d63cc4fd388a23edd6e
김태은 <kbo6924@naver.com>
2025-03-10 11:37:30 +0900

    데이터베이스 파일 객체 데이터베이스 문자열경로, 실제 파일 분리 저장완료

--- src/main/java/com/example/team01/book/service/BookServiceImple.java
+++ src/main/java/com/example/team01/book/service/BookServiceImple.java
@@ -32,28 +32,42 @@ public class BookServiceImple implements BookService{
     @Override
     public int createBook(BookVO book) {
         
-        String bookImgPath="";
+        String bookImgPath=""; // 데이터베이스에 담을 파일명 담는 문자열 변수
         int cnt =0;
         if(book != null) {
             log.info("createBook에 파일 객체도 담겨서 넘어옴:{}", book );
-            log.info("book.file?????:{}", book.getBookImgPath()); // 1개 이상의 파일
+            log.info("book.file?????:{}", book.getBookImgPath()); // 1개 이상의 파일 객체 (파일 날데이터)
 
             if(!book.getBookImgPath().isEmpty()) {
                 for (MultipartFile file : book.getBookImgPath()) {
                     String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename(); //랜덤 파일 명칭(중복방지)
                     log.info("fileName:{}", fileName);
 
+                    
+                    //2. 데이터베이스에 파일 저장( 
                     if(!bookImgPath.equals("")) {
                         bookImgPath= bookImgPath +","+fileName;
                     }else{
                         bookImgPath = fileName;
                     }
+
                     //실제파일을 프로젝트 내부에(서버) 저장
-                    Path path = Paths.get(bookImgPath);
-                    log.info("bookImgPath-------------------:{}", path);
+                    //여기에서 실제 경로에 파일저장
+                    String saveFilePath = "upload" + File.separator + "book" + File.separator + fileName;//운영체제에 맞게 파일 경로 생성하기 위한 코드
+                    Path path = Paths.get(saveFilePath); //파라미터에 해당하는 파일 경로를 가져오는 Path 변수로, 해당 파일을 저장할 파일명을 포함한 경로를 의미
+                    log.info("path-------------------:{}", path);
+                    try {
+                        //upload 디렉토리가 없는 경우 noSuchFileExcption 방지
+                        Files.createDirectories(path.getParent()); // 디렉터리(폴더) 없으면 생성
+                        Files.write(path, file.getBytes()); // 파일 저장
+                        log.info("파일 저장 완료: {}", saveFilePath);
+                    } catch (IOException e) {
+                        log.error("파일 저장 실패: {}", e.getMessage());
+                    }
+
                 }
                 // List<String> bookImgPath를 하나의 문자열로 변환
-                log.info("bookImgPath---------------------:{}", bookImgPath); // 리스트 객체
+                log.info("bookImgPath---------------------데이터베이스에 저장할 문자열:{}", bookImgPath); // 리스트 객체
                 // 객체를 또 순회해서 문자열로 만들어야함 ?????
             }else{
                 //null일 떄
