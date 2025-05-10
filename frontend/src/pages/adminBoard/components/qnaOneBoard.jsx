422da83df32cb848702168757507c3f292790a04
JongHoSke <astra9706@naver.com>
2025-05-10 12:33:48 +0900

    메인브랜치 최신화

--- frontend/src/pages/adminBoard/components/qnaOneBoard.jsx
+++ frontend/src/pages/adminBoard/components/qnaOneBoard.jsx
@@ -1,3 +1,4 @@
+import "@assets/css/board/oneBoard.css";
 const QnaOneBoard = () => {
 
 
@@ -6,22 +7,29 @@ const QnaOneBoard = () => {
             <h3>1:1 문의 목록</h3>
             <div>
                 <div>
-                    <div><textarea></textarea></div>
+                    <ul className="oneBoardIndex">
+                        <li className="item">번호</li>
+                        <li className="item">제목</li>
+                        <li className="item">작성자</li>
+                        <li className="item">상태</li>
+                        <li className="item">등록일</li>
+                    </ul>
+                </div>
+                <div className="oneBoardQuestionBox">
+                    <ul>
+                        <li>문의제목</li>
+                        <li>문의2</li>
+                        <li>문의3</li>
+                        <li>문의4</li>
+                    </ul>
+                </div>
+                <div className="boardQnaFinder">
+                    <div className="boardQnaFinderBox">
+                        <textarea className="Finder_textArea" placeholder="검색어 입력">
+                        </textarea>
+                    </div>
                     <button>검색</button>
                 </div>
-                <ul>
-                    <li>번호</li>
-                    <li>제목</li>
-                    <li>작성자</li>
-                    <li>등록일</li>
-                    <li>상태</li>
-                </ul>
-                <ul>
-                    <li>문의제목</li>
-                    <li>문의2</li>
-                    <li>문의3</li>
-                    <li>문의4</li>
-                </ul>
             </div>
             <div>
                 <button>삭제</button>
