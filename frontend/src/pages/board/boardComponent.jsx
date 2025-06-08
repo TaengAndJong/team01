69a41939b75a4da359a0a0eebc47a72f36eae740
JongHoSke <astra9706@naver.com>
2025-06-08 21:41:26 +0900

    사용자 페이지 레프트 메뉴 마이페이지 참고 후 수정

--- frontend/src/pages/board/boardComponent.jsx
+++ frontend/src/pages/board/boardComponent.jsx
@@ -1,9 +1,11 @@
 import "@assets/css/board/userBoard.css"
 import Btn from "@util/reuseBtn.jsx";
 import pathsData from "@assets/pathsData.jsx";
-import {Outlet, useLocation} from "react-router-dom";
+import {Link, Outlet, useLocation,} from "react-router-dom";
 import React from "react";
 import LeftMenu from "@layout/LeftMenu.jsx";
+import {useMenu} from "../common/MenuContext.jsx";
+import {menuNavi} from "../../util/menuNavi.jsx";
 
 //context 상태관리
 export const BoardStateContext = React.createContext();// state 값을 공급하는 context
@@ -12,19 +14,79 @@ export const PaginationContext = React.createContext();
 
 
 const Board = () => {
+
     const location = useLocation();
     const isCreatePage = location.pathname.includes("/board/createBoard");
     const logCheck = () =>{
         console.log("페이지 이동")
     }
 
+    const {currentPath}  = useMenu();
+    console.log("currentPath",currentPath);
 
+    //마이페이지 타이틀 동적 설정 함수
+    const clientTile=(currentPath)=>{
+        //마지막 주소값 받도록
+        const lastPathArr = currentPath.split("/"); // 주소값을 "/"  기준으로 배열반환
+        const lastPathNm= lastPathArr[lastPathArr.length-1] // 제일 마지막 배열값 인덱스 설정
+        //switch 문으로 제목 지정
+        switch(lastPathNm){
+            case "oneboard":
+                return "1:1문의";
+                break;
+            case "productboard":
+                return "상품문의";
+                break;
+            case "diliveryboard":
+                return "배송문의";
+                break;
+            default:
+                return "게시판";
+        }
+    }
     return (
         <div>
-            <div className="page d-flex">
-                <div className="left">
-                    <LeftMenu/>
-                </div>
+            <div className="hoverLeaf"></div>
+            <div className="page client d-flex">
+                <aside id="sidebar" className="left-menu">
+                    <div>
+                        <div className="left-title">
+                            {/*현재경로의 부모 1차메뉴 이름*/}
+                            <h4 className="sub-title first-title title-border">
+                                {clientTile(currentPath)}
+                            </h4>
+
+                            <ul className="depth first-depth">
+
+                                <li>
+                                    <Link to="oneboard"
+                                          className={`depth-menu first ${currentPath.includes("oneboard") ? "current" : ""}`}>
+                                        1:1문의
+                                        <i className="leaf icon"></i>
+                                    </Link>
+
+                                </li>
+                                <li>
+                                    <Link to="productboard"
+                                          className={`depth-menu first ${currentPath.includes("productBoard") ? "current" : ""}`}>
+                                        배송지
+                                        <i className="leaf icon"></i>
+                                    </Link>
+
+                                </li>
+                                <li>
+                                    <Link to="diliveryboard"
+                                          className={`depth-menu first ${currentPath.includes("deliveryBoard") ? "current" : ""}`}>
+                                        상품문의
+                                        <i className="leaf icon"></i>
+                                    </Link>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </aside>
+                {/*링크이동할 사이드메뉴 */}
                 <div>
                     {!isCreatePage && (
                         <>
@@ -45,9 +107,8 @@ const Board = () => {
                     </BoardStateContext.Provider>
                 </div>
             </div>
+        </div>
+    );
+};
 
-            </div>
-            );
-            };
-
-            export default Board;
+export default Board;
