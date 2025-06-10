bdba93e350082846d3921a46271de7266443ed91
JongHoSke <astra9706@naver.com>
2025-06-10 16:44:18 +0900

    브랜치 최신화용 커밋

--- frontend/src/pages/board/boardComponent.jsx
+++ frontend/src/pages/board/boardComponent.jsx
@@ -36,6 +36,7 @@ const Board = () => {
     const {currentPath}  = useMenu();
     console.log("currentPath",currentPath);
 
+
     //마이페이지 타이틀 동적 설정 함수
     const clientTile=(currentPath)=>{
         //마지막 주소값 받도록
--- frontend/src/pages/board/components/createBoardComponent.jsx
+++ frontend/src/pages/board/components/createBoardComponent.jsx
@@ -3,10 +3,30 @@ import Btn from "@util/reuseBtn.jsx";
 import {useContext} from "react";
 import {UserDataContext} from "../boardComponent.jsx";
 import {maskUserId} from "@util/maskingID.jsx";
+import {useState} from "react";
 
 const CreateBoardComponent = ()=> {
+    const userData = useContext(UserDataContext); // context 사용
 
-    const userData = useContext(UserDataContext);
+    const [formData, setFormData] = useState({
+        category: "",
+        title:"",
+        content: "",
+        files:[],
+    }); // 문의 데이터 저장 할 state
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+    console.log("저장되는 데이터" , formData);
+
+    const postHandler = async () => { // 게시물 등록 버튼
+
+    }
 
     return(
         <>
@@ -17,45 +37,43 @@ const CreateBoardComponent = ()=> {
                         <dd>
                             <span>{userData.clientName}</span>
                         </dd>
-
                     </div>
                     <div className="d-flex">
                         <dt className="id">ID</dt>
                         <dd>
                             <span>{maskUserId(userData.clientId)}</span>
                         </dd>
-
                     </div>
                     <div className="d-flex">
                         <dt>문의 종류</dt>
                         <dd>
                             <div>
-                                <select className="inquireOption">
-                                    <option className="">문의 선택</option>
-                                    <option className="">1:1 문의</option>
-                                    <option className="">상품 문의</option>
-                                    <option className="">배송 문의</option>
+                                <select className="inquireOption" name="category" onChange={handleChange}>
+                                    <option value="">문의 선택</option>
+                                    <option value="">1:1 문의</option>
+                                    <option value="">상품 문의</option>
+                                    <option value="">배송 문의</option>
                                 </select>
                             </div>
                         </dd>
 
                     </div>
                     <div className="d-flex">
-                        <dt className="inquireTitle">문의 제목</dt>
-                        <dd><input/></dd>
+                        <dt className="inquireTitle" >문의 제목</dt>
+                        <dd><input name="title" onChange={handleChange}/></dd>
 
                     </div>
 
                     <div className="d-flex">
                         <dt className="inquireContents">문의 내용</dt>
-                        <dd><textarea/></dd>
+                        <dd><textarea name="content" onChange={handleChange}/></dd>
                     </div>
 
                     <div className="d-flex">
                         <dt className="attachfiles">첨부 파일</dt>
                         <dd>
                             <p>이미지 파일만 첨부해 주세요</p>
-                            <a className="btn">파일 첨부하기</a>
+                            <a className="btn" onClick={() => fileRef.current.click()}>파일 첨부하기</a>
                             <span>"(<b>최대 10개</b>, 30MB)"</span>
                         </dd>
                     </div>
