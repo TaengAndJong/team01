109c9b9f33f9891ff55b2fb8d3f80ffd9f59461d
JongHoSke <astra9706@naver.com>
2025-06-12 14:45:35 +0900

    최신화용 커밋

--- src/main/java/com/example/team01/client/service/CreateBoardService.java
+++ src/main/java/com/example/team01/client/service/CreateBoardService.java
@@ -1,5 +1,14 @@
 package com.example.team01.client.service;
 
-public interface CreateBoardService {
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import com.example.team01.vo.CreateBoardVO;
 
+@Slf4j
+@Service
+
+public interface CreateBoardService {
+   public void createBoard(String category, String title, String content, MultipartFile file);
 }
