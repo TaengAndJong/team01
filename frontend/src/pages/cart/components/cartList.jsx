f2051e7ad2749062fd0c887c0f3e2c8b4e3fb570
김태은 <kbo6924@naver.com>
2025-06-28 14:51:23 +0900

    장바구니 체크박스 작업 완료

--- frontend/src/pages/cart/components/cartList.jsx
+++ frontend/src/pages/cart/components/cartList.jsx
@@ -1,5 +1,5 @@
 import "@assets/css/cart/cartList.css"
-import {useContext, useEffect, useState} from "react";
+import {useCallback, useContext, useEffect, useState} from "react";
 import {CartStateContext} from "../cartComponent.jsx";
 import CartAddress from "./cartAddress.jsx";
 import CartAccount from "./cartAccount.jsx";
@@ -11,17 +11,57 @@ const CartList = () => {
         console.log("컴포넌트 렌더링 - cartData:", cartData);
         // 구조분해 할당을 통한 bookList ==> 구조분해 할당 시,cartdata에 담긴 키명 그대로 받아야함
         const {bookList, address} = cartData?.[0] || [];
-
         // 삭제 , 갱신 등의 데이터 조작이 필요한 경우 상태관리 변수 사용
         const [cartList, setCartList] = useState(null);
 
+        //목록선택 개별상태관리변수
+        const [selectItem, setSelectItem] = useState([]);
+
+
+        //개별선택 핸들러
+        const selectOneHandler = (cartId,checked)=>{
+            console.log("selectOne-- 개별선택", cartId);
+            console.log("selectOne-- checked", checked);
+            //checked가 true 이면
+            if(checked){
+                console.log("선택 true");
+                //배열로 선택된 아이디 장바구니아이템 담기
+                setSelectItem((prev)=>[
+                    ...prev, // 배열이 담긴 cartId
+                    cartId, // 새로 선택된 cartId
+                ]);
+            }else{
+                console.log("선택 false");
+                //체크가 해제된 cartId를 제외하고 배열에 반환(필터링)
+                setSelectItem((prev)=> prev.filter(item => item !== cartId)) // filter 함수는 배열로 값 반환
+            }
+        }
+
+
+        //전체선택 핸들러
+        const selectAllHandler = (checkedAll)=>{
+            console.log("selectAll-- 전체선택", checkedAll);
+            console.log("selectAll-- selectItem", selectItem);
+            // 이미 담긴 아이디는 담지 않는다 ==> 필터링
+            if(checkedAll){ //전체선택 checked가 true이면
+                //cartList의 모든 아이디 순회하고
+                //selectItem에 이미 담긴 아이디는 제외한 Id 담기
+                const filterSelectIds= cartList.map((item)=> item.cartId) // cartList의 item의 cartId를  map 함수로 배열로 반환 후
+                    .filter(id => id !== selectItem.includes(id)); // 개별선택으로 배열에 담긴 아이디와 동일하지 않은 Id만 필터링해서 반환
+                console.log("filterSelectIds", filterSelectIds);
+                //selectItem에 데이터 갱신
+                setSelectItem(filterSelectIds);
+            }else{
+                setSelectItem([]); // 빈 배열 리셋
+            }
+        }
+         console.log("selectItem----------Last",selectItem);
         //cartData가 변화할 때마다 데이터 갱신
         useEffect(() => {
             if (cartData) {
                 setCartList(bookList);
             }
         },[cartData])
-
 
 
         //장바구니 데이터가 빈 배열(빈 값)일 경우 UI반환 함수
@@ -36,71 +76,73 @@ const CartList = () => {
             )
         }
 
-    const addCartList = (cartList) => {
+        const addCartList = (cartList) => {
 
-        return (
-      <>
-          {/*label 내부에 input 기입 시, htmlFor 기입 불필요*/}
-          <div className="d-flex justify-content-between align-items-center mb-4">
-              <label className="">
-                  <input type="checkbox" name="check-all" aria-label="전체 선택"/>
-                  전체선택
-              </label>
-              <button aria-label="삭제하기" className="cart btn btn-danger">삭제하기</button>
-          </div>
+            return (
+          <>
+              {/*label 내부에 input 기입 시, htmlFor 기입 불필요*/}
+              <div className="d-flex justify-content-between align-items-center mb-4">
+                  <label className="">
+                      <input type="checkbox" name="check-all" aria-label="전체 선택" onChange={(e)=>{selectAllHandler(e.target.checked)}}/>
+                      전체선택
+                  </label>
+                  <button aria-label="삭제하기" className="cart btn btn-danger">삭제하기</button>
+              </div>
 
-          <ul className="cart-list">
-              {cartList?.map((item,index) => (
-                  <li key={index} className="cart-item mb-2">
-                      <div className="item-inner d-flex card flex-row default-border position-relative p-4 mb-2 ">
-                          <label className="position-absolute check-one" htmlFor={`check0${index+1}`}>
-                              <input type="checkbox" id={`check0${index+1}`}>
-                              </input>선택삭제</label>
-                          <div className="card-header border-end rounded-4 overflow-hidden">
-                              <div className="img-box">
-                                  <div className="img-inner">
-                                      <img className="img" src={`${item.bookVO.bookImgList[0]}`} alt="노이미지"/>
+              <ul className="cart-list">
+                  {cartList?.map((item,index) => (
+                      <li key={index} className="cart-item mb-2">
+                          <div className="item-inner d-flex card flex-row default-border position-relative p-4 mb-2 ">
+                              <label className="position-absolute check-one" htmlFor={`check0${index+1}`}>
+                                  <input type="checkbox" id={`check0${index+1}`} onChange={(e)=>{selectOneHandler(item.cartId,e.target.checked)}}>
+                                  </input>선택삭제
+                              </label>
+                              <div className="card-header border-end rounded-4 overflow-hidden">
+                                  <div className="img-box">
+                                      <div className="img-inner">
+                                          <img className="img" src={`${item.bookVO.bookImgList[0]}`} alt="노이미지"/>
+                                      </div>
                                   </div>
                               </div>
+                              {/* 도서 정보*/}
+                              <div className="bookInfo card-body">
+                                  <strong className="book-title title-dotted d-block">{item.bookVO.bookName}</strong>
+                                  <ul className="ul bullet">
+                                      <li className="li"><span className="tit">저자</span>{item.bookVO.author}</li>
+                                      <li className="li"><span className="tit">발행일</span>{item.bookVO.publishDate}</li>
+                                      <li className="li"><span className="tit">가격</span><em>{item.bookVO.bookPrice}</em>원</li>
+
+                                  </ul>
+                                  {/* 도서 가격 */}
+                                  <ul className="cart-item-count ul bullet border-top border-bottom py-3 mt-5 d-flex">
+                                      <li className="li d-inline-flex  align-items-center pe-3">
+                                          <span className="tit me-4">상품금액</span>
+                                          <span className="price"><em>20,000</em>원</span>
+                                      </li>
+                                      <li className="li d-inline-flex align-items-center px-3">
+                                          <span className="tit me-4">배송금액</span>
+                                          <span className="price"><em>2,000</em>원</span>
+                                      </li>
+                                      <li className="d-inline-flex align-items-center ms-auto">
+                                          <button type="submit" aria-label="구매하기"
+                                                  className="submit btn btn-secondary">선택도서구매
+                                          </button>
+                                      </li>
+                                  </ul>
+                                  {/* 도서 */}
+                              </div>
                           </div>
-                          {/* 도서 정보*/}
-                          <div className="bookInfo card-body">
-                              <strong className="book-title title-dotted d-block">{item.bookVO.bookName}</strong>
-                              <ul className="ul bullet">
-                                  <li className="li"><span className="tit">저자</span>{item.bookVO.author}</li>
-                                  <li className="li"><span className="tit">발행일</span>{item.bookVO.publishDate}</li>
-                                  <li className="li"><span className="tit">가격</span><em>{item.bookVO.bookPrice}</em>원</li>
 
-                              </ul>
-                              {/* 도서 가격 */}
-                              <ul className="cart-item-count ul bullet border-top border-bottom py-3 mt-5 d-flex">
-                                  <li className="li d-inline-flex  align-items-center pe-3">
-                                      <span className="tit me-4">상품금액</span>
-                                      <span className="price"><em>20,000</em>원</span>
-                                  </li>
-                                  <li className="li d-inline-flex align-items-center px-3">
-                                      <span className="tit me-4">배송금액</span>
-                                      <span className="price"><em>2,000</em>원</span>
-                                  </li>
-                                  <li className="d-inline-flex align-items-center ms-auto">
-                                      <button type="submit" aria-label="구매하기"
-                                              className="submit btn btn-secondary">선택도서구매
-                                      </button>
-                                  </li>
-                              </ul>
-                              {/* 도서 */}
-                          </div>
-                      </div>
+                      </li>
+                  ))}
 
-                  </li>
-              ))}
+              </ul>
 
-          </ul>
-
-      </>
-        )
-    }
+          </>
+            )
+        }
         console.log("addrCartlist --- cartList",address);
+
     return (
         <>
             <div className="cart d-block clearfix">
