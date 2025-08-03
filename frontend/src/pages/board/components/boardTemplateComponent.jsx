ce626ddabe0a0e350026b02322384805bfcd9a93
JongHoSke <astra9706@naver.com>
2025-08-03 11:26:55 +0900

    게시판 데이터 레이아웃 데이터 정렬 및 기타 수정 완료

--- frontend/src/pages/board/components/boardTemplateComponent.jsx
+++ frontend/src/pages/board/components/boardTemplateComponent.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import BoardListComponent from "./boardListComponent";
 import { BoardListContext } from "../boardComponent";
+import PropTypes from "prop-types";
+import "@assets/css/board/userBoard.css";
 
 const BoardTemplateComponent = ({ category }) => {
   const boardList = useContext(BoardListContext);
@@ -10,9 +12,25 @@ const BoardTemplateComponent = ({ category }) => {
 
   return (
     <div>
-      <BoardListComponent categoryListData={list} />
+      <div>
+        <ul className="BoardIndex">
+          <li className="item">제목</li>
+          <li className="item">작성자</li>
+          <li className="item">상태</li>
+          <li className="item">등록일</li>
+        </ul>
+      </div>
+      <div>
+        {list.map((item, index) => (
+          <BoardListComponent key={index} categoryListData={item} />
+        ))}
+      </div>
     </div>
   );
+};
+
+BoardTemplateComponent.propTypes = {
+  category: PropTypes.string.isRequired,
 };
 
 export default BoardTemplateComponent;
