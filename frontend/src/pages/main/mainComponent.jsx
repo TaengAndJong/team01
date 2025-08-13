fcd9c0952c87b472b7270117256dfed8ec4de375
JongHoSke <astra9706@naver.com>
2025-08-13 19:55:54 +0900

    메인 컴포넌트 주석 처리

--- frontend/src/pages/main/mainComponent.jsx
+++ frontend/src/pages/main/mainComponent.jsx
@@ -1,69 +1,60 @@
-
-import "@assets/css/main.css"
-import {Link} from "react-router-dom";
+import "@assets/css/main.css";
+import { Link } from "react-router-dom";
 import MainBookSlide from "./components/mainBookSlide.jsx";
-import SectionMain from "./components/sectionMain.jsx";
-import SectionBooks from "./components/sectionBooks.jsx";
-import SectionSkipBtns from "./components/sectionSkipBtns.jsx";
-import SectionCuration from "./components/SectionCuration.jsx";
-import {useEffect, useState} from "react";
+// import SectionMain from "./components/sectionMain.jsx";
+// import SectionBooks from "./components/sectionBooks.jsx";
+// import SectionSkipBtns from "./components/sectionSkipBtns.jsx";
+// import SectionCuration from "./components/SectionCuration.jsx";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-
 const mainComponent = () => {
+  //const [slideData,setSlideData] = useState([]); // slide data 배열로 초기값
+  //const [crData,setCrData] = useState([]); // slide data 배열로 초기값
 
-    //const [slideData,setSlideData] = useState([]); // slide data 배열로 초기값
-    //const [crData,setCrData] = useState([]); // slide data 배열로 초기값
-
-
-    const booksFecth =  async () =>{
-
-        //axios 요청 보내기
-        //api, { bookSlide: true} , api?bookSlide 이런식으로 보내서 컨트롤러에서 매핑처리하기 ( 시큐리티에 안걸림)
-        try{
-            const response = await axios.get("/api/bookSlide");
-            console.log("response data",response.data)
-
-        }catch(e){
-            console.log("booksFetch 에러");
-            console.error(e);
-        }
+  const booksFecth = async () => {
+    //axios 요청 보내기
+    //api, { bookSlide: true} , api?bookSlide 이런식으로 보내서 컨트롤러에서 매핑처리하기 ( 시큐리티에 안걸림)
+    try {
+      const response = await axios.get("/api/bookSlide");
+      console.log("response data", response.data);
+    } catch (e) {
+      console.log("booksFetch 에러");
+      console.error(e);
     }
+  };
 
-
-    const curationFecth = async () =>{
-        //axios 요청 보내기
-        try{
-            const response = await axios.get("/api/curation");
-            console.log("response data",response.data)
-
-        }catch(e){
-            console.log("curationFecth 에러");
-            console.error(e);
-        }
-
+  const curationFecth = async () => {
+    //axios 요청 보내기
+    try {
+      const response = await axios.get("/api/curation");
+      console.log("response data", response.data);
+    } catch (e) {
+      console.log("curationFecth 에러");
+      console.error(e);
     }
+  };
 
-    //마운트 시 1번 실행
-    useEffect(()=>{
-        console.log("main 컴포넌트 마운트시작");
-        booksFecth();
-       // curationFecth();
-        console.log("main 컴포넌트 마운트끝");
-    },[]);
+  //마운트 시 1번 실행
+  useEffect(() => {
+    console.log("main 컴포넌트 마운트시작");
+    booksFecth();
+    // curationFecth();
+    console.log("main 컴포넌트 마운트끝");
+  }, []);
 
-
-    return (<>
-            {/* 메인*/}
-            <SectionMain/>
-            {/*도서 슬라이드*/}
-            <SectionBooks/>
-            {/* 문의, 위시리스트 , 회원가입 */}
-            <SectionSkipBtns/>
-            {/*인기도서 & 추천도서 */}
-            <SectionCuration/>
-        </>
-    )
-}
+  return (
+    <>
+      {/* 메인*/}
+      <SectionMain />
+      {/*도서 슬라이드*/}
+      <SectionBooks />
+      {/* 문의, 위시리스트 , 회원가입 */}
+      <SectionSkipBtns />
+      {/*인기도서 & 추천도서 */}
+      <SectionCuration />
+    </>
+  );
+};
 
 export default mainComponent;
