006dc8344204aa0fe1887e7938ccf12799faa267
JongHoSke <astra9706@naver.com>
2025-09-12 21:14:51 +0900

    사용자 삭제 api 작성 중 백업

--- src/main/java/com/example/team01/vo/OneBoardVO.java
+++ src/main/java/com/example/team01/vo/OneBoardVO.java
@@ -5,7 +5,7 @@ import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.Serializable;
 import java.util.List;
-
+import java.time.LocalDateTime;
 
 @ToString
 @Getter
@@ -13,15 +13,29 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OneBoardVO implements Serializable {
-    private String qnaOneId; // 게시물 번호
-    private String qnaTitle; // 게시물 제목
-    private String qnaContent; // 게시물 내용
-    private String clientId; // 사용자 Id
-    private String qnaStatus; // 게시물 상태
-    private String qnaDate; // 게시물 작성일    
-    private String qnaWriter; // 사용자 실명
-    private String category; // 게시물 카테고리
-    private List<MultipartFile> files; // 첨부파일 (다중)
-    private String attachmentID; // 저장된 파일명들 (ex: "a.jpg,b.jpg")
+    private String qnaOneId; // 1:1 문의 index
+    private String qnaTitle; // 1:1 문의 제목
+    private String qnaContent; // 1:1 문의 내용
+    private String qnaWriter; // 1:1 문의 작성자
+    private LocalDateTime qnaDate; // 1:1 문의 날짜 현재시간으로
+    private LocalDateTime qnaDel; // 1:1 문의 삭제 여부
+    private String qnaStatus; // 1:1 문의 답변 상태
+    private String clientId; // 1:1 문의 작성자 아이디
+    private String roleId; // 1:1 문의 작성자 권한
+    private String attachmentID; // 1:1 문의 첨부 이미지
+    private List<AttachmentVO> attachmentList; // 첨부 이미지 리스트 get으로 요청 시
+    private CommentsVO comment; // 단일 댓글 반환
+
+    // private String qnaOneId; // 게시물 번호
+    // private String qnaTitle; // 게시물 제목
+    // private String qnaContent; // 게시물 내용
+    // private String clientId; // 사용자 Id
+    // private String qnaStatus; // 게시물 상태
+    // private String qnaDate; // 게시물 작성일    
+    // private String qnaWriter; // 사용자 실명
+    // private String category; // 게시물 카테고리
+    // private List<MultipartFile> files; // 첨부파일 (다중)
+    // private String attachmentID; // 저장된 파일명들 (ex: "a.jpg,b.jpg")
+    // private CommentsVO comment; // 단일 댓글 반환
 }
 
