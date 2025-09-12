8c864c1686a7ae3526f4ebd7064d1b607917b2f2
김태은 <kbo6924@naver.com>
2025-09-12 23:04:15 +0900

    예외처리 클ㄹ래스 수정

--- frontend/src/pages/book/components/addCartBtn.jsx
+++ frontend/src/pages/book/components/addCartBtn.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 const AddCartBtn = ({bookId,quantity}) =>{
 
@@ -10,27 +11,26 @@ const AddCartBtn = ({bookId,quantity}) =>{
         console.log("bookId---------sendCartFetch",bookId);
         console.log("quantity-----------sendCartFetch",quantity);
         try{
-            const response = await fetch("/api/cart",{
-                method:"POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                credentials: 'include',
-                body: JSON.stringify({
-                    bookId: bookId,
-                    quantity: quantity
-                })
-            })
-
-            if(!response.ok){
-                throw new Error("Failed to fetch cart list",response.statusText);
-            }
-
-            const data = await response.json();
-            console.log("data cart List ", data);
-
+         const response = await axios.post("/api/cart",
+             {bookId:bookId,quantity:quantity} // 서버로 보내는 데이터
+            , { withCredentials: true }//쿠키허용
+         );
+            console.log("장바구니 추가 비동기요청 ",response.data);
+            alert(`장바구니 담기 성공`);
         }catch(err){
-            console.log("장바구니에 담기 실패",err);
+            if (err.response) {
+                // 서버가 응답은 했지만 상태코드가 400, 401, 403, 500 등
+                console.error("서버 상태 코드:", err.response.status);
+                console.error("서버 메시지---:", err.response.data);
+                console.error("서버 메시지---:", err.response.data.message);
+                alert(`주문가능한 수량은 ${err.response.data.maxQuantity}개 입니다.`);
+            } else if (err.request) {
+                // 요청은 했는데 서버가 응답이 없음
+                console.error("서버 응답 없음:", err.request);
+            } else {
+                // 기타 에러
+                console.error("요청 설정 에러:", err.message);
+            }
         }
     }
 
