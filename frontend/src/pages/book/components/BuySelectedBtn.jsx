62961eb843313800970672f77f928aeeea6b9548
김태은 <kbo6924@naver.com>
2025-09-18 14:43:00 +0900

    bookCount 수량변경 초기값 undefined 일 경우 1로 설정되게 수정

--- frontend/src/pages/book/components/BuySelectedBtn.jsx
+++ frontend/src/pages/book/components/BuySelectedBtn.jsx
@@ -8,9 +8,9 @@ const BuySelectedBtn = ({type, book,cartId}) => {
 
     const params = {type,book,cartId};
     const navigate = useNavigate();
-    console.log("구매하기 --------book",book);
-    console.log("구매하기--------type",type);
-    console.log("단건구매 장바구니아이디--------cartId",cartId);
+    // console.log("구매하기 --------book",book);
+    // console.log("구매하기--------type",type);
+    // console.log("단건구매 장바구니아이디--------cartId",cartId);
     //단건구매일때에는 cartId도 같이 결제페이지로 넘겨줘야함
     // 단건구매(selected)일 경우에만 cartId 추가
     if (type === "selected" && cartId) {
@@ -21,8 +21,8 @@ const BuySelectedBtn = ({type, book,cartId}) => {
 
     //선택구매 버튼을 누르면 결제페이지로 이동되고, 도서정보도 같이 넘겨줘야함
     const gotoPayment = async (book,type) => {
-        console.log(`book ${book}, type ${type}`);
-        console.log(`cartId ${cartId}`);
+        // console.log(`book ${book}, type ${type}`);
+        // console.log(`cartId ${cartId}`);
         // 도서정보, 버튼type, cartId
         // 결제페이지로 파라미터를 담아서 이동하기
         navigate("/payment",{
