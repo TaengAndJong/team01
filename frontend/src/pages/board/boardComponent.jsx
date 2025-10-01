cbc14102126be92c2c0fa4695d90c6cd59d7a0a5
JongHoSke <astra9706@naver.com>
2025-10-01 23:32:53 +0900

    백업2

--- frontend/src/pages/board/boardComponent.jsx
+++ frontend/src/pages/board/boardComponent.jsx
@@ -4,6 +4,10 @@ import React, { useEffect, useState } from "react";
 import { useMenu } from "../common/MenuContext.jsx";
 import axios from "axios";
 import { useAuth } from "@pages/common/AuthContext.jsx";
+import "@layout/LeftMenu";
+import LeftMenu from "./../../layout/LeftMenu";
+import { menuNavi } from "../../util/menuNavi.jsx";
+
 //context 상태관리
 export const BoardStateContext = React.createContext(); // state 값을 공급하는 context
 export const BoardDispatchContext = React.createContext(); // 생성, 수정(갱신), 삭제 값을 공급하는 context
@@ -21,9 +25,8 @@ const Board = () => {
   }); // 사용자의 게시물 목록 저장 할 state
   const [refreshTrigger, setRefreshTrigger] = useState(0);
   // 메뉴 경로 관리
-  const { currentPath } = useMenu();
+  const { menu, currentPath, standardPoint } = useMenu(); // menuProvider에서 데이터를 제공하는 커스텀훅
 
-  // 사용자 게시물 별 데이터 조회
   const fetchData = async () => {
     console.log("userData fetchData", userData);
     try {
@@ -52,23 +55,6 @@ const Board = () => {
     fetchData();
   }, [userData]);
 
-  const clientTile = (currentPath) => {
-    //마지막 주소값 받도록
-    const lastPathArr = currentPath.split("/"); // 주소값을 "/"  기준으로 배열반환
-    const lastPathNm = lastPathArr[lastPathArr.length - 1]; // 제일 마지막 배열값 인덱스 설정
-    //switch 문으로 제목 지정
-    switch (lastPathNm) {
-      case "oneBoard":
-        return "1:1문의";
-      case "productBoard":
-        return "상품문의";
-      case "deliveryBoard":
-        return "배송문의";
-      default:
-        return "게시판";
-    }
-  };
-
   // 새로고침 트리거 핸들러
   const handleRefreshTrigger = () => {
     setRefreshTrigger((prev) => prev + 1);
@@ -82,75 +68,82 @@ const Board = () => {
     }
   }, [refreshTrigger]);
 
+  let clientMenuTree = menuNavi(menu?.clientList);
+  let clientHome = menu?.clientList?.find(
+    (item) => item.menuId === "main"
+  )?.menuPath;
+  let subNavi = clientMenuTree?.filter((item) =>
+    item.menuPath.includes(standardPoint)
+  );
+  console.log("메뉴트리", clientMenuTree);
+  console.log("홈", clientHome);
+  console.log("서브메뉴", subNavi);
+  console.log("스탠", standardPoint);
   return (
     <div>
       <div className="hoverLeaf"></div>
       <div className="page client d-flex">
-        <aside id="sidebar" className="left-menu">
-          <div>
-            <div className="left-title">
-              {/*현재경로의 부모 1차메뉴 이름*/}
-              <h4 className="sub-title first-title title-border">
-                {clientTile(currentPath)}
-              </h4>
-
-              <ul className="depth first-depth">
-                <li>
-                  <Link
-                    to="oneBoard"
-                    className={`depth-menu first ${
-                      currentPath.includes("oneBoard") ? "current" : ""
-                    }`}
-                  >
-                    1:1문의
-                    <i className="leaf icon"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="productBoard"
-                    className={`depth-menu first ${
-                      currentPath.includes("productBoard") ? "current" : ""
-                    }`}
-                  >
-                    상품문의
-                    <i className="leaf icon"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="deliveryBoard"
-                    className={`depth-menu first ${
-                      currentPath.includes("deliveryBoard") ? "current" : ""
-                    }`}
-                  >
-                    배송문의
-                    <i className="leaf icon"></i>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </aside>
+        <div className="left">
+          <LeftMenu />
+        </div>
         {/*링크이동할 사이드메뉴 */}
-
         <div className="right">
           <section className="content custom-border">
-            <BoardStateContext.Provider value={null}>
-              <BoardDispatchContext.Provider value={null}>
-                <PaginationContext.Provider value={null}>
-                  <UserDataContext.Provider value={userData}>
-                    <BoardListContext.Provider value={boardList}>
-                      <BoardRefreshTriggerContext.Provider
-                        value={handleRefreshTrigger}
-                      >
-                        <Outlet />
-                      </BoardRefreshTriggerContext.Provider>
-                    </BoardListContext.Provider>
-                  </UserDataContext.Provider>
-                </PaginationContext.Provider>
-              </BoardDispatchContext.Provider>
-            </BoardStateContext.Provider>
+            <div className="content-inner">
+              {/*현재경로의 페이지명 depth 2 */}
+              <h3 className="sub-title current-title title-border">
+                {menu?.clientList?.map((item) => {
+                  if (item.menuPath.startsWith(`${currentPath}`)) {
+                    return item.menuName;
+                  }
+                })}
+              </h3>
+
+              {/*depth별 네비주소,현재페이지일 경우 표시필요*/}
+
+              <ol className="menu-navi d-flex title-border">
+                {/* 서브페이지 네비게이션 */}
+                <li>
+                  <Link to={clientHome} className="home icon">
+                    <span className="sr-only">홈</span>
+                  </Link>
+                </li>
+                {subNavi?.[0] && (
+                  <li>
+                    <Link to={subNavi?.[0].menuPath}>
+                      {subNavi?.[0].menuName}
+                    </Link>
+                  </li>
+                )}
+                {subNavi?.[0]?.secondChild?.length > 0 && (
+                  <li>
+                    {" "}
+                    {subNavi?.[0].secondChild
+                      ?.filter(
+                        (item) =>
+                          item.menuDepth === "2차메뉴" &&
+                          item.menuPath.includes(currentPath)
+                      )
+                      .map((item) => item.menuName)}
+                  </li>
+                )}
+              </ol>
+              <BoardStateContext.Provider value={null}>
+                <BoardDispatchContext.Provider value={null}>
+                  <PaginationContext.Provider value={null}>
+                    <UserDataContext.Provider value={userData}>
+                      <BoardListContext.Provider value={boardList}>
+                        <BoardRefreshTriggerContext.Provider
+                          value={handleRefreshTrigger}
+                        >
+                          <Outlet />
+                        </BoardRefreshTriggerContext.Provider>
+                      </BoardListContext.Provider>
+                    </UserDataContext.Provider>
+                  </PaginationContext.Provider>
+                </BoardDispatchContext.Provider>
+              </BoardStateContext.Provider>
+            </div>
           </section>
         </div>
       </div>
