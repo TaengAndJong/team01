008d35e1ad46d7a6a12861bc45b68d8a8d9bd7d2
JongHoSke <astra9706@naver.com>
2025-10-10 22:06:44 +0900

    백엔드 관리자, 사용자 보드, 첨부파일, 답변  Dao 및 Mapper 수정

--- src/main/java/com/example/team01/comments/dao/CommentsDao.java
+++ src/main/java/com/example/team01/comments/dao/CommentsDao.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface CommentsDao {
     public int insertComment(CommentsVO commentsVO);
-    public CommentsVO getCommentById(String qnaRefId, String commentType);
+    public CommentsVO getCommentById(@Param("qnaRefId") String qnaRefId,@Param("commentType") String commentType);
     public int postCommentUpdate(CommentsVO commentsVO);
     public int postCommentDelete(@Param("commentId") String commentId);
 }
