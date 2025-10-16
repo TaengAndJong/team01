131dd36688f94b647c24ab9a50d58ff7092b1fe4
김태은 <kbo6924@naver.com>
2025-10-16 21:14:15 +0900

    장바구니 상품 중복처리, 중복시 모달 알림 작업완료, 찜목록 장바구니버튼 활성화

--- src/main/java/com/example/team01/cart/service/CartService.java
+++ src/main/java/com/example/team01/cart/service/CartService.java
@@ -12,7 +12,7 @@ public interface CartService {
 
     
     // 도서의 수량 확인
-    public CartVO checkBookCount(String bookId);
+   // public CartVO checkBookCount(String bookId);
 
     //장바구니에 담을 도서 정보 조회
     public CartVO selectBookInfo(String bookId);
@@ -22,6 +22,9 @@ public interface CartService {
 
     //장바구니에 담긴 클라이언트별 장바구니 목록 조회
     public List<CartDTO> selectUserBookList(String clientId);
+
+    //장바구니에 담긴 해당 클라이언트의 특정상품 중복 조회
+    public int selectDuplicateCheck(String clientId,String bookId);
 
     //장바구니에서 삭제
     public int deleteToCartList(List<String> deleteIds);
