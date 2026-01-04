3f5ee66f142404e1a1e15f233506bc07b1d45649
김태은 <kbo6924@naver.com>
2026-01-04 11:15:09 +0900

    도서추천타입, 판매상태 수정초기값 적용, 파일 용량 제한 수정 및 최대용량표시 추가 , 서버 파일용량(스프링옵션 복구

--- src/main/java/com/example/team01/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/team01/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.team01.common.exception;
 
 
+import com.example.team01.common.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,13 +18,13 @@ public class GlobalExceptionHandler {
 
     //특정예외처리 메서드
     @ExceptionHandler(CustomCartException.class)
-    public ResponseEntity<Map<String, Object>> handleCartException(CustomCartException ex) {
+    public ResponseEntity<Map<String, Object>> handleCartException(CustomCartException e) {
         log.info("비즈니스 예외 발생--- 장바구니 수량부족예외 handleCartException");
 
         Map<String, Object> result = new HashMap<>();
-        result.put("bookId", ex.getBookId());
-        result.put("message", ex.getMessage());
-        result.put("maxQuantity", ex.getStock());
+        result.put("bookId", e.getBookId());
+        result.put("message", e.getMessage());
+        result.put("maxQuantity", e.getStock());
 
         return ResponseEntity
                 .badRequest() // 400
@@ -33,8 +34,8 @@ public class GlobalExceptionHandler {
     // 특정예외처리시 메서드 추가
     // 파일 없음 예외 처리
     @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleNoResourceFound(NoResourceFoundException ex) {
-        log.warn("파일 없음 발생: {}", ex.getMessage());
+    public ResponseEntity<Map<String, Object>> handleNoResourceFound(NoResourceFoundException e) {
+        log.warn("파일 없음 발생: {}", e.getMessage());
 
         Map<String, Object> result = new HashMap<>();
         result.put("message", "요청한 파일을 서버에서 찾을 수 없습니다.");
@@ -45,10 +46,22 @@ public class GlobalExceptionHandler {
                 .body(result);
     }
 
-    // 그 외 일반 예외 처리
+    //비즈니스 로직 예외만 처리 ( 400 Bad Request )
+    public ResponseEntity<Map<String, Object>> handleBusinessException(BusinessException e){
+        log.warn("비즈니스로직 예외발생: {}", e.getMessage());
+        
+        Map<String, Object> result = new HashMap<>();
+        result.put("message", e.getMessage());
+
+        return ResponseEntity
+                .badRequest()  // 400 상태 코드
+                .body(result);
+    }
+
+    // 그 외 일반 예외 처리 ( 500 )
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleException(Exception ex) {
-        log.error("서버 에러 발생: {}", ex.getMessage(), ex);
+    public ResponseEntity<Map<String, Object>> handleException(Exception e) {
+        log.error("서버 에러 발생: {}", e.getMessage(), e);
 
         Map<String, Object> result = new HashMap<>();
         result.put("message", "서버에서 처리 중 오류가 발생했습니다.");
