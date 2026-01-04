9b3fc2b04da0f4f7246d83f4457a7a635d25cef5
JongHoSke <astra9706@naver.com>
2026-01-04 19:12:34 +0900

    위시 오류 수정 프론트, 백엔드

--- frontend/src/pages/book/components/bookItem.jsx
+++ frontend/src/pages/book/components/bookItem.jsx
@@ -1,153 +1,181 @@
-import {Link, useNavigate} from "react-router-dom";
-import React, {useRef, useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useRef, useState } from "react";
 import AddCartBtn from "@pages/book/components/addCartBtn.jsx";
 import BuySelectedBtn from "./BuySelectedBtn.jsx";
 import BookCount from "./bookCount.jsx";
 import WishBtn from "./wishBtn.jsx";
 import axios from "axios";
-import {useAuth} from "@common/AuthContext.jsx";
+import { useAuth } from "@common/AuthContext.jsx";
 import ImgBaseUrl from "@/util/imgBaseUrl";
 
 /*
-* 1) 조회에 대한 서버요청 줄이기 위한 조회 및 중복조회  처리
-*
-* */
+ * 1) 조회에 대한 서버요청 줄이기 위한 조회 및 중복조회  처리
+ *
+ * */
 
+const BookItem = ({ bookList, wishIds, setWishIds }) => {
+  //구매할 도서 수량 상태관리 객체 ==> 아이디별로 도서수량 저장하기 위해 {} 빈 객체로 초기값 설정
+  const [bookCount, setBookCount] = useState({});
+  const viewBookRef = useRef(new Set()); // 세션 내 중복 클릭 방지
+  const navigate = useNavigate();
+  const userData = useAuth(); // 로그인 인증 정보
 
-const BookItem = ({bookList,wishIds,setWishIds}) =>{
+  //console.log("북아이템 자식 컴포넌트 wishIds",wishIds);
+  // console.log("북아이템 자식 컴포넌트 bookCount",bookCount);
+  console.log("북아이템 자식 컴포넌트 bookList", bookList);
 
-    //구매할 도서 수량 상태관리 객체 ==> 아이디별로 도서수량 저장하기 위해 {} 빈 객체로 초기값 설정
-    const [bookCount,setBookCount]=useState({});
-    const viewBookRef = useRef(new Set());// 세션 내 중복 클릭 방지
-    const navigate = useNavigate();
-    const userData = useAuth(); // 로그인 인증 정보
+  //로그인 정보가 있을경우
 
-    //console.log("북아이템 자식 컴포넌트 wishIds",wishIds);
-   // console.log("북아이템 자식 컴포넌트 bookCount",bookCount);
-  console.log("북아이템 자식 컴포넌트 bookList",bookList);
+  //상세페이지로 이동시 link 태그 ( a 태그 ) 로 사용자가 조회한 bookId 서버로 전송하는 핸들러
+  const viewBookFetch = async (e, bookId) => {
+    e.preventDefault(); // link 태그 이동이벤트 방지 ==> 이벤트가 발생하면 비동기요청이 처리되지 않을 수 있음
 
-    //로그인 정보가 있을경우
-
-    //상세페이지로 이동시 link 태그 ( a 태그 ) 로 사용자가 조회한 bookId 서버로 전송하는 핸들러
-    const viewBookFetch = async(e, bookId) => {
-        e.preventDefault();// link 태그 이동이벤트 방지 ==> 이벤트가 발생하면 비동기요청이 처리되지 않을 수 있음
-
-       //서버로 전송된 도서, 로그인 상태 여부
-        if(viewBookRef.current.has(bookId) || !userData.isAuthenticated) {
-            //서버전송 안하고 상세페이지로 이동
-            navigate(`/book/bookDetail/${bookId}`);
-            return; // 종료
-        }
-        //없는 bookId 이면 ref에 추가
-        viewBookRef.current.add(bookId);
-        //서버로 booId전송
-        //formData 또는 쿼리스트링일 때만 RequestParam, jSON boody는 RequestBody
-        try{
-            const response = await axios.post("/api/viewBook",{bookId});
-          //  console.log("도서조회시 도서 아이디 전송에 대한 응답",response);
-
-        }catch(error){
-            console.log("조회된 도서 Id 전송 에러 ", error);
-
-        }finally{//최종적으로 실행 ==> 해당도서의 상세페이지로 이동
-            console.log("상세페이지로 이동 : ",bookId);
-            navigate(`/book/bookDetail/${bookId}`);
-        }
-
+    //서버로 전송된 도서, 로그인 상태 여부
+    if (viewBookRef.current.has(bookId) || !userData.isAuthenticated) {
+      //서버전송 안하고 상세페이지로 이동
+      navigate(`/book/bookDetail/${bookId}`);
+      return; // 종료
     }
-
-
-    const recomTypeMap = {
-        NORMAL: { recomType: "normal", label: "일반" },
-        RECOMMEND: { recomType: "recom", label: "추천" },
-        POPULAR: { recomType: "popular", label: "인기" },
-    };
-
-    const recomTultip = (status) => {
-        console.log(
-            `status : ${status} , recomtype : ${recomTypeMap[status]?.recomType},label: ${recomTypeMap[status]?.label}`
-        );
-
-        return (
-            <span className={`tultip d-inline-flex ${recomTypeMap[status]?.recomType}`}>
-        {recomTypeMap[status]?.label}
-      </span>
-        );
-    };
-
-    const saleStatus=(status)=>{
-        switch (status) {
-            case "판매중": return (<span className="tultip d-inline-flex">{status}</span>);
-            case "미판매": return (<span className="tultip d-inline-flex popular">{status}</span>);
-            case "단종": return (<span className="tultip d-inline-flex complete">{status}</span>);
-            default:return "판매상태 알수없음";
-        }
+    //없는 bookId 이면 ref에 추가
+    viewBookRef.current.add(bookId);
+    //서버로 booId전송
+    //formData 또는 쿼리스트링일 때만 RequestParam, jSON boody는 RequestBody
+    try {
+      const response = await axios.post("/api/viewBook", { bookId });
+      //  console.log("도서조회시 도서 아이디 전송에 대한 응답",response);
+    } catch (error) {
+      console.log("조회된 도서 Id 전송 에러 ", error);
+    } finally {
+      //최종적으로 실행 ==> 해당도서의 상세페이지로 이동
+      console.log("상세페이지로 이동 : ", bookId);
+      navigate(`/book/bookDetail/${bookId}`);
     }
+  };
 
+  const recomTypeMap = {
+    NORMAL: { recomType: "normal", label: "일반" },
+    RECOMMEND: { recomType: "recom", label: "추천" },
+    POPULAR: { recomType: "popular", label: "인기" },
+  };
 
+  const recomTultip = (status) => {
+    console.log(
+      `status : ${status} , recomtype : ${recomTypeMap[status]?.recomType},label: ${recomTypeMap[status]?.label}`
+    );
 
     return (
-        <div className="book-list-inner overflow-hidden">
-            {bookList && (
-                <ul className="book-item-list clearfix d-flex flex-wrap">
-                    {bookList?.map((book, index) => (
-                        <li key={index} className="book-item mb-3 mx-2 overflow-hidden justify-content-between">
-                            <Link to={`/book/bookDetail/${book.bookId}`} className="book-link d-block" id={book.bookId} onClick={(e)=>viewBookFetch(e,book.bookId)}>
-                                <div className="item-inner d-flex card flex-row  position-relative">
-                                    <div className="card-header border-end rounded-4 overflow-hidden">
-                                        <div className="img-box">
-                                            <div className="img-inner">
-                                                <img className="img" src={ImgBaseUrl(book.bookImgList[0])}
-                                                     alt="노이미지"/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="bookInfo card-body">
-                                        <strong className="book-title title-dotted d-block">
-                                            {recomTultip(book.recomType)}
-                                            {saleStatus(book.saleStatus)}
-                                            <span>{book.bookName}</span>
-                                        </strong>
-                                        <ul className="ul bullet">
-                                            <li className="li"><span className="tit fw-bold">저자</span>{book.author}</li>
-                                            <li className="li"><span className="tit fw-bold">발행일</span>{book.publishDate}</li>
-                                            {/*<li className="li"><span className="tit">출판사</span>{book.publishDate}</li>*/}
-                                            <li className="li"><span className="tit fw-bold">가격</span><span>{book.bookPrice}</span>원
-                                            </li>
-                                            <li className="li"><span className="tit fw-bold">배송비</span><span>2,000</span>원
-                                            </li>
-                                        </ul>
-
-                                    </div>
-                                </div>
-                            </Link>
-                            {/* 수량 및 액션 버튼 영역 */}
-                            <div className="item-inner d-flex align-items-center justify-content-end mt-4">
-
-                                <WishBtn wishIds={wishIds} setWishIds={setWishIds} bookId={book.bookId} />
-                                {book.stock === 0 ?(
-                                    <p>품절</p>
-                                ):(
-                                    <>
-                                        {/*수량*/}
-                                        <BookCount bookId={book.bookId} bookCount={bookCount} setBookCount={setBookCount} />
-                                        {/*장바구니추가*/}
-                                        <AddCartBtn bookId={book.bookId} quantity={bookCount[book.bookId] ?? 1}  />
-                                        {/*바로구매*/}
-                                        <BuySelectedBtn  type={"buyNow"}  book={ {...book, quantity: bookCount[book.bookId] ?? 1} } />
-                                    </>
-                                    )}
-                            </div>
-
-                        </li>
-
-                    ))}
-
-                </ul>
-            )}
-
-        </div>
+      <span
+        className={`tultip d-inline-flex ${recomTypeMap[status]?.recomType}`}
+      >
+        {recomTypeMap[status]?.label}
+      </span>
     );
-}
+  };
+
+  const saleStatus = (status) => {
+    switch (status) {
+      case "판매중":
+        return <span className="tultip d-inline-flex">{status}</span>;
+      case "미판매":
+        return <span className="tultip d-inline-flex popular">{status}</span>;
+      case "단종":
+        return <span className="tultip d-inline-flex complete">{status}</span>;
+      default:
+        return "판매상태 알수없음";
+    }
+  };
+
+  return (
+    <div className="book-list-inner overflow-hidden">
+      {bookList && (
+        <ul className="book-item-list clearfix d-flex flex-wrap">
+          {bookList?.map((book) => (
+            <li
+              key={book.bookId}
+              className="book-item mb-3 mx-2 overflow-hidden justify-content-between"
+            >
+              <Link
+                to={`/book/bookDetail/${book.bookId}`}
+                className="book-link d-block"
+                id={book.bookId}
+                onClick={(e) => viewBookFetch(e, book.bookId)}
+              >
+                <div className="item-inner d-flex card flex-row  position-relative">
+                  <div className="card-header border-end rounded-4 overflow-hidden">
+                    <div className="img-box">
+                      <div className="img-inner">
+                        <img
+                          className="img"
+                          src={ImgBaseUrl(book.bookImgList[0])}
+                          alt="노이미지"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bookInfo card-body">
+                    <strong className="book-title title-dotted d-block">
+                      {recomTultip(book.recomType)}
+                      {saleStatus(book.saleStatus)}
+                      <span>{book.bookName}</span>
+                    </strong>
+                    <ul className="ul bullet">
+                      <li className="li">
+                        <span className="tit fw-bold">저자</span>
+                        {book.author}
+                      </li>
+                      <li className="li">
+                        <span className="tit fw-bold">발행일</span>
+                        {book.publishDate}
+                      </li>
+                      {/*<li className="li"><span className="tit">출판사</span>{book.publishDate}</li>*/}
+                      <li className="li">
+                        <span className="tit fw-bold">가격</span>
+                        <span>{book.bookPrice}</span>원
+                      </li>
+                      <li className="li">
+                        <span className="tit fw-bold">배송비</span>
+                        <span>2,000</span>원
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </Link>
+              {/* 수량 및 액션 버튼 영역 */}
+              <div className="item-inner d-flex align-items-center justify-content-end mt-4">
+                <WishBtn
+                  wishIds={wishIds}
+                  setWishIds={setWishIds}
+                  bookId={book.bookId}
+                />
+                {book.stock === 0 ? (
+                  <p>품절</p>
+                ) : (
+                  <>
+                    {/*수량*/}
+                    <BookCount
+                      bookId={book.bookId}
+                      bookCount={bookCount}
+                      setBookCount={setBookCount}
+                    />
+                    {/*장바구니추가*/}
+                    <AddCartBtn
+                      bookId={book.bookId}
+                      quantity={bookCount[book.bookId] ?? 1}
+                    />
+                    {/*바로구매*/}
+                    <BuySelectedBtn
+                      type={"buyNow"}
+                      book={{ ...book, quantity: bookCount[book.bookId] ?? 1 }}
+                    />
+                  </>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default BookItem;
--- frontend/src/pages/book/components/wishBtn.jsx
+++ frontend/src/pages/book/components/wishBtn.jsx
@@ -1,136 +1,164 @@
-import React, {useState} from "react";
-import {useAuth} from "../../common/AuthContext.jsx";
-import {useNavigate} from "react-router-dom";
-import {useModal} from "../../common/modal/ModalContext.jsx";
-import {catchError} from "../../../util/error.jsx";
+import React, { useState } from "react";
+import { useAuth } from "../../common/AuthContext.jsx";
+import { useNavigate } from "react-router-dom";
+import { useModal } from "../../common/modal/ModalContext.jsx";
+import { catchError } from "../../../util/error.jsx";
 import axios from "axios";
 
-const WishBtn = ({wishIds,setWishIds,bookId}) =>{
-    // 로그인 여부
-    const { isAuthenticated } = useAuth(); //
-    //이동
-    const navigate = useNavigate();
-    //모달
-    const {openModal,closeModal}= useModal();
-    //서버에서 클라이언트까지 응답이 도달할 때까지의 상태관리
-    const [isLoading, setIsLoading] = useState(false);
+const WishBtn = ({ wishIds, setWishIds, bookId }) => {
+  // 로그인 여부
+  const { isAuthenticated } = useAuth(); //
+  //이동
+  const navigate = useNavigate();
+  //모달
+  const { openModal, closeModal } = useModal();
+  //서버에서 클라이언트까지 응답이 도달할 때까지의 상태관리
+  const [isLoading, setIsLoading] = useState(false);
 
-    // console.log("isAuthenticated",isAuthenticated);
-    // console.log("wishIds",wishIds);
+  // console.log("isAuthenticated",isAuthenticated);
+  // console.log("wishIds",wishIds);
 
+  console.log(
+    "wishIds:",
+    wishIds,
+    "bookId:",
+    bookId,
+    "includes:",
+    wishIds.includes(bookId),
+    "types:",
+    typeof wishIds[0],
+    typeof bookId
+  );
+  //찜목록 비동기 fetch 요청
+  const wishFetch = async (bookId) => {
+    //   console.log("wishFetch --- 진입")
+    // bookId는 쿼리스트링보다 body 에 담아주는게 더 나음
+    const UrlSearchParams = new URLSearchParams();
+    UrlSearchParams.append("bookId", bookId);
+    // console.log("bookId wishList",bookId);
 
-    //찜목록 비동기 fetch 요청
-    const wishFetch = async(bookId) =>{
-     //   console.log("wishFetch --- 진입")
-        // bookId는 쿼리스트링보다 body 에 담아주는게 더 나음
-        const UrlSearchParams = new URLSearchParams();
-        UrlSearchParams.append("bookId", bookId);
-       // console.log("bookId wishList",bookId);
+    try {
+      //경로에 bookId 담아서 보내기
+      const response = await axios.post(
+        `/api/mypage/wishlist/save?${UrlSearchParams.toString()}`,
+        { bookId: bookId }
+      );
 
-        try{
-            //경로에 bookId 담아서 보내기
-            const response = await axios.post(`/api/mypage/wishlist/save?${UrlSearchParams.toString()}`,{bookId:bookId});
+      // console.log("찜목록 페치 응답",response);
 
-          // console.log("찜목록 페치 응답",response);
+      if (response.status === 200) {
+        // wishIds 값 재설정할 때에 이전 상태값에 대해서 bookId가 있는 지 확인후 동일한 bookId 이면 필터링해서 새배열반환, 없으면 기존배열을 복사 후 bookId 추가
+        setWishIds((prev) => {
+          // 이전 상태값에 bookId가 있는지 확인
+          const existId = prev.includes(bookId);
 
-
-            if(response.status === 200){
-                // wishIds 값 재설정할 때에 이전 상태값에 대해서 bookId가 있는 지 확인후 동일한 bookId 이면 필터링해서 새배열반환, 없으면 기존배열을 복사 후 bookId 추가
-                setWishIds((prev) =>
-                    {
-                        // 이전 상태값에 bookId가 있는지 확인
-                        const existId = prev.includes(bookId);
-
-                        // 찜목록 모달 분기 기준 (이미 담겨져 있을경우 재요청은 찜해제요청)
-                        if(existId){
-                            openModal({
-                                modalType:"error",
-                                content:<><p>찜목록 삭제 성공.</p></>,
-                                onConfirm: () => {closeModal();},
-                            });
-                            // 기존 존재하는 아이디를 제외하고 배열 반환
-                            return  prev.filter((id) => id !== bookId) // 제거;
-                        }else{
-                            // 존재하지 않는다면 추가 (찜목록 추가)
-                            openModal({
-                                modalType: "error",
-                                content: <><p>찜목록에 추가되었습니다.</p></>,
-                                onConfirm: () => closeModal(),
-                            });
-                            // 기존 배열에 새로운 찜목록 도서 아이디 추가
-                            return [...prev,bookId];
-                        }
-                    }
-                );
-
-            }
-
-        }catch(err){
-            //에러 처리 핸들러
-            catchError(err, { openModal, closeModal});
-        }
+          // 찜목록 모달 분기 기준 (이미 담겨져 있을경우 재요청은 찜해제요청)
+          if (existId) {
+            openModal({
+              modalType: "error",
+              content: (
+                <>
+                  <p>찜목록 삭제 성공.</p>
+                </>
+              ),
+              onConfirm: () => {
+                closeModal();
+              },
+            });
+            // 기존 존재하는 아이디를 제외하고 배열 반환
+            return prev.filter((id) => id !== bookId); // 제거;
+          } else {
+            // 존재하지 않는다면 추가 (찜목록 추가)
+            openModal({
+              modalType: "error",
+              content: (
+                <>
+                  <p>찜목록에 추가되었습니다.</p>
+                </>
+              ),
+              onConfirm: () => closeModal(),
+            });
+            // 기존 배열에 새로운 찜목록 도서 아이디 추가
+            return [...prev, bookId];
+          }
+        });
+      }
+    } catch (err) {
+      //에러 처리 핸들러
+      catchError(err, { openModal, closeModal });
+    }
     //wishFetch 엔드
-    }
+  };
 
-    //찜목록 핸들러
-    const wishHandler= async (bookId)=>{
+  //찜목록 핸들러
+  const wishHandler = async (bookId) => {
+    // 클릭하면 selected 클래스 추가,
+    //찜버튼 누르면 전송 되기 전까지 disable 되게  해야하나?
+    if (isLoading) return; // 클라이언트가 서버로 정보 요청 중이면 중단
+    setIsLoading(true); // 클라이언트가 서버로 요청 중
 
-        // 클릭하면 selected 클래스 추가,
-        //찜버튼 누르면 전송 되기 전까지 disable 되게  해야하나?
-        if (isLoading) return;      // 클라이언트가 서버로 정보 요청 중이면 중단
-        setIsLoading(true);         // 클라이언트가 서버로 요청 중
+    /*  withCredentials: true
+     *  CORS 로 설정되어 있으면, 기존 프론트 포트와 백엔드 포트가 다르고
+     * 기본적으로 쿠키가 포함되어있지 않기 때문에
+     * withCredentials: true 설정을 통해 쿠키를 같이 비동기 요청에 담아
+     * 서버에서 세션 정보를 확인하고 서버에서 쿠기기반 세션확인을 통해 로그인 상태 인증을 가능하게 함
+     *  */
 
-        /*  withCredentials: true
-        *  CORS 로 설정되어 있으면, 기존 프론트 포트와 백엔드 포트가 다르고
-        * 기본적으로 쿠키가 포함되어있지 않기 때문에
-        * withCredentials: true 설정을 통해 쿠키를 같이 비동기 요청에 담아  
-        * 서버에서 세션 정보를 확인하고 서버에서 쿠기기반 세션확인을 통해 로그인 상태 인증을 가능하게 함
-        *  */
+    //bookId를 받아서 비동기요청, 컨트롤러로 전송
+    try {
+      if (!isAuthenticated) {
+        openModal({
+          modalType: "confirm",
+          content: (
+            <>
+              <p>로그인페이지로 이동하시겠습니까?</p>
+            </>
+          ),
+          onConfirm: () => {
+            closeModal();
+            navigate("/login");
+          },
+        });
 
-        //bookId를 받아서 비동기요청, 컨트롤러로 전송
-        try{
-            if (!isAuthenticated) {
-                openModal({
-                    modalType:"confirm",
-                    content:<><p>로그인페이지로 이동하시겠습니까?</p></>,
-                    onConfirm: () => {closeModal(); navigate("/login");}
-                });
+        return;
+      } else {
+        //로그인 중인지  세션 체크검증필요
+        const response = await axios.get("/api/auth", {
+          withCredentials: true,
+        });
+        //console.log("response status",response);
 
-                return;
-            }else{
-                //로그인 중인지  세션 체크검증필요
-                const response = await axios.get("/api/auth",{ withCredentials: true });
-                //console.log("response status",response);
-
-                if(response.status === 200) {
-                    // console.log("찜목록 페치요청 보내는 듕");
-                    // console.log("엑시오스 찜목록 버튼",response.status);
-                    await wishFetch(bookId);
-                }
-            }
-
-        }catch(err){
-            // console.error("찜 토글 실패", err);
-            // console.error("서버 상태 코드:", err.response.status);
-            // console.error("서버 메시지---데이터:", err.response.data);
-            // console.error("서버 메시지--- 메시지:", err.response.data.message);
-            //에러 처리 핸들러
-            catchError(err, { openModal, closeModal,navigate});
-        } finally {
-            setIsLoading(false);      // 응답 완료되면 다시 클릭 가능하게 설정
+        if (response.status === 200) {
+          // console.log("찜목록 페치요청 보내는 듕");
+          // console.log("엑시오스 찜목록 버튼",response.status);
+          await wishFetch(bookId);
         }
+      }
+    } catch (err) {
+      // console.error("찜 토글 실패", err);
+      // console.error("서버 상태 코드:", err.response.status);
+      // console.error("서버 메시지---데이터:", err.response.data);
+      // console.error("서버 메시지--- 메시지:", err.response.data.message);
+      //에러 처리 핸들러
+      catchError(err, { openModal, closeModal, navigate });
+    } finally {
+      setIsLoading(false); // 응답 완료되면 다시 클릭 가능하게 설정
     }
+  };
 
-
-    return (
-        <>
-            <button type="button"
-                    className={`submit btn me-2 icon wish ${wishIds.includes(bookId) ? "selected" : ""}`}
-                    onClick={() => wishHandler(bookId)}>
-                <span className="sr-only">위시리스트에 담기</span>
-            </button>
-        </>
-    )
-}
+  return (
+    <>
+      <button
+        type="button"
+        className={`submit btn me-2 icon wish ${
+          wishIds.includes(bookId) ? "selected" : ""
+        }`}
+        onClick={() => wishHandler(bookId)}
+      >
+        <span className="sr-only">위시리스트에 담기</span>
+      </button>
+    </>
+  );
+};
 
 export default WishBtn;
