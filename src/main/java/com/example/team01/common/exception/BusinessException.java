4bae5f7f9397545207ee475a98ed9973331889cf
김태은 <kbo6924@naver.com>
2026-01-19 15:08:31 +0900

    리패고링 하기전 백업

--- src/main/java/com/example/team01/common/exception/BusinessException.java
+++ src/main/java/com/example/team01/common/exception/BusinessException.java
@@ -3,7 +3,8 @@ package com.example.team01.common.exception;
 public class BusinessException extends RuntimeException {
 
     public BusinessException(String message) {
-       super(message); // 런타임예외를 상속받고 있어서 선언만 해주면 됨
+
+        super(message); // 런타임예외를 상속받고 있어서 선언만 해주면 됨
     }
     // 새로 커스텀하려면, 클래스변수를 선언하고 
     // 기존 부모로부터 상속받은 message 파라미터와 새로운 파라미터를 사용한 
